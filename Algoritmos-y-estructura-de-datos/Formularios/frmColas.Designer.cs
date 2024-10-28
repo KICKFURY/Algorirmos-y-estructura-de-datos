ae3b6195f8f046de732cd80566a478b8f0e39589
KICKFURY <guzmancito16032018dqtc@gmail.com>
2024-10-27 22:04:43 -0600

    Cambios realizados para el Colas: Se incorporaron campos para el cliente y mas detalles para el producto

--- Algoritmos-y-estructura-de-datos/Formularios/frmColas.Designer.cs
+++ Algoritmos-y-estructura-de-datos/Formularios/frmColas.Designer.cs
@@ -32,14 +32,21 @@
             this.listBoxInventario = new System.Windows.Forms.ListBox();
             this.txtProducto = new System.Windows.Forms.TextBox();
             this.btnProcesar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnAgregar
             // 
-            this.btnAgregar.Location = new System.Drawing.Point(12, 48);
+            this.btnAgregar.Location = new System.Drawing.Point(17, 186);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(75, 23);
-            this.btnAgregar.TabIndex = 0;
+            this.btnAgregar.TabIndex = 4;
             this.btnAgregar.Text = "Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
             this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
@@ -47,38 +54,104 @@
             // listBoxInventario
             // 
             this.listBoxInventario.FormattingEnabled = true;
-            this.listBoxInventario.Location = new System.Drawing.Point(203, 12);
+            this.listBoxInventario.Location = new System.Drawing.Point(235, 12);
             this.listBoxInventario.Name = "listBoxInventario";
-            this.listBoxInventario.Size = new System.Drawing.Size(163, 199);
+            this.listBoxInventario.Size = new System.Drawing.Size(282, 199);
             this.listBoxInventario.TabIndex = 1;
             // 
             // txtProducto
             // 
-            this.txtProducto.Location = new System.Drawing.Point(51, 12);
+            this.txtProducto.Location = new System.Drawing.Point(12, 29);
             this.txtProducto.Name = "txtProducto";
-            this.txtProducto.Size = new System.Drawing.Size(100, 20);
-            this.txtProducto.TabIndex = 2;
+            this.txtProducto.Size = new System.Drawing.Size(95, 20);
+            this.txtProducto.TabIndex = 0;
             // 
             // btnProcesar
             // 
-            this.btnProcesar.Location = new System.Drawing.Point(109, 48);
+            this.btnProcesar.Location = new System.Drawing.Point(114, 186);
             this.btnProcesar.Name = "btnProcesar";
             this.btnProcesar.Size = new System.Drawing.Size(75, 23);
-            this.btnProcesar.TabIndex = 0;
+            this.btnProcesar.TabIndex = 5;
             this.btnProcesar.Text = "Facturar";
             this.btnProcesar.UseVisualStyleBackColor = true;
             this.btnProcesar.Click += new System.EventHandler(this.btnFacturar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Producto";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(114, 29);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(95, 20);
+            this.txtCantidad.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(134, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Cantidad";
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(12, 77);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(95, 20);
+            this.txtCliente.TabIndex = 2;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(114, 77);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(95, 20);
+            this.txtPrecio.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(31, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Cliente";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(142, 61);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Precio";
             // 
             // frmColas
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(378, 226);
+            this.ClientSize = new System.Drawing.Size(541, 226);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtCliente);
+            this.Controls.Add(this.txtCantidad);
             this.Controls.Add(this.txtProducto);
             this.Controls.Add(this.listBoxInventario);
             this.Controls.Add(this.btnProcesar);
             this.Controls.Add(this.btnAgregar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmColas";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frmColas";
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -91,5 +164,12 @@
         private System.Windows.Forms.ListBox listBoxInventario;
         private System.Windows.Forms.TextBox txtProducto;
         private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
--- Algoritmos-y-estructura-de-datos/Formularios/frmColas.cs
+++ Algoritmos-y-estructura-de-datos/Formularios/frmColas.cs
@@ -10,14 +10,23 @@ using System.Windows.Forms;
 
 namespace Algoritmos_y_estructura_de_datos.Formularios
 {
+
+    public struct Inventario
+    {
+        public string nombreProducto { get; set; }
+        public string nombreCliente { get; set; }
+        public int cantidadProducto { get; set; }
+        public double precioProducto { get; set; }
+    }
+
     public partial class frmColas : Form
     {
-        private Queue<string> pedidos;
+        private Queue<Inventario> pedidos;
 
         public frmColas()
         {
             InitializeComponent();
-            pedidos = new Queue<string>();
+            pedidos = new Queue<Inventario>();
         }
 
         protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
@@ -31,11 +40,25 @@ namespace Algoritmos_y_estructura_de_datos.Formularios
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            if (!string.IsNullOrEmpty(txtProducto.Text))
-                pedidos.Enqueue(txtProducto.Text);
+            if (!string.IsNullOrEmpty(txtProducto.Text) && !string.IsNullOrEmpty(txtCliente.Text) &&
+                int.TryParse(txtCantidad.Text, out int cantidad) && double.TryParse(txtPrecio.Text, out double precio))
+            {
+                Inventario nuevoPedido = new Inventario
+                {
+                    nombreProducto = txtProducto.Text,
+                    nombreCliente = txtCliente.Text,
+                    cantidadProducto = cantidad,
+                    precioProducto = precio
+                };
+
+                pedidos.Enqueue(nuevoPedido);
                 ActualizarLista();
                 txtProducto.Clear();
-                MessageBox.Show("Producto agregado exitosamente", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtCliente.Clear();
+                txtCantidad.Clear();
+                txtPrecio.Clear();
+                MessageBox.Show("Producto agregado exitosamente", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
 
         private void btnFacturar_Click(object sender, EventArgs e)
@@ -46,15 +69,18 @@ namespace Algoritmos_y_estructura_de_datos.Formularios
                 return;
             }        
                 
-            string producto = pedidos.Dequeue();
-            MessageBox.Show($"Facturado {producto}", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            Inventario producto = pedidos.Dequeue();
+            MessageBox.Show($"Facturada a {producto.nombreCliente}", "Informacion", MessageBoxButtons.OK, MessageBoxIcon.Information);
             ActualizarLista();
         }
 
         private void ActualizarLista()
         {
             listBoxInventario.Items.Clear();
-            listBoxInventario.Items.AddRange(pedidos.ToArray());
+            foreach (var pedido in pedidos)
+            {
+                listBoxInventario.Items.Add($"{pedido.nombreProducto} - {pedido.nombreCliente} - {pedido.cantidadProducto} unidades - ${pedido.precioProducto} - Subtotal ${pedido.precioProducto * pedido.cantidadProducto}");
+            }
         }
     }
 }
